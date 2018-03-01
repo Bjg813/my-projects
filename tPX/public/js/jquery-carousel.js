1b18c5f2e53f336ca738ff14dbc0a7c978f4d3ef
Brennangrout <bjg813@gmail.com>
2018-02-28 19:00:11 -0800

    fixing carousel

--- tPX/public/js/jquery-carousel.js
+++ tPX/public/js/jquery-carousel.js
@@ -1,80 +1,108 @@
 $(document).ready(function() {
-    (function ($) {
-        //slideshow interval
-        var autoSwap = setInterval( swap,10000);
+   //slideshow style interval
+    var autoSwap = setInterval( swap,3500);
+
+    //pause slideshow and reinstantiate on mouseout
+    $('ul, span').hover(
+    function () {
+        clearInterval(autoSwap);
+    }, 
+    function () {
+    autoSwap = setInterval( swap,3500);
+    });
+
+    //global variables
+    var items = [];
+    var startItem = 1;
+    var position = 0;
+    var itemCount = $('.carousel li.items').length;
+    var leftpos = itemCount;
+    var resetCount = itemCount;
+
+    //unused: gather text inside items class
+    $('li.items').each(function(index) {
+        items[index] = $(this).text();
+    });
+
+    //swap images function
+    function swap(action) {
+    var direction = action;
     
-        //pause/unpause slideshow on mouse hover
-        $('ul, span').hover(
-              function () {
-            clearInterval(autoSwap);
-        },
+    //moving carousel backwards
+    if(direction == 'counter-clockwise') {
+        var leftitem = $('.left-pos').attr('id') - 1;
+        if(leftitem == 0) {
+        leftitem = itemCount;
+        }
+        
+        $('.right-pos').removeClass('right-pos').addClass('back-pos');
+        $('.main-pos').removeClass('main-pos').addClass('right-pos');
+        $('.left-pos').removeClass('left-pos').addClass('main-pos');
+        $('#'+leftitem+'').removeClass('back-pos').addClass('left-pos');
+        
+        startItem--;
+        if(startItem < 1) {
+        startItem = itemCount;
+        }
+    }
     
-        function () {
-           autoSwap = setInterval( swap,10000);
-        });
-    
-        //variables
-        var items = [];
-        var startItem = 1;
-        var position = 0;
-        var itemCount = $('.carousel li.items').length;
-        var leftpos = itemCount;
-        var resetCount = itemCount;
-    
-        //swap images function
-        function swap(action) {
-              var direction = action;
-          
-              //previous button
-              if(direction == 'counter-clockwise') {
-                var leftitem = $('.left-pos-2').attr('id') - 1;
-                console.log(leftitem);
-                if(leftitem <= 0) {
-                  leftitem = itemCount;
-                  console.log(leftitem);
-                }
-                console.log('prev');
-                $('.right-pos-2').removeClass('right-pos-2').addClass('back-pos');
-                $('.right-pos').removeClass('right-pos').addClass('right-pos-2');
-                $('.main-pos').removeClass('main-pos').addClass('right-pos');
-                $('.left-pos').removeClass('left-pos').addClass('main-pos');
-                $('.left-pos-2').removeClass('left-pos-2').addClass('left-pos');
-                $('#'+leftitem).removeClass('back-pos').addClass('left-pos-2');
+    //moving carousel forward
+    if(direction == 'clockwise' || direction == '' || direction == null ) {
+        function pos(positionvalue) {
+        if(positionvalue != 'leftposition') {
+            //increment image list id
+            position++;
+            
+            //if final result is greater than image count, reset position.
+            if((startItem+position) > resetCount) {
+            position = 1-startItem;
             }
-          
-              //next button
-              if(direction == 'clockwise' || direction == '' || direction == null ) {
-                var rightitem = $('.right-pos-2').attr('id');
-                    rightitem++;
-                console.log(rightitem);
-                if(rightitem > itemCount) {
-                  rightitem = 1;
-                  console.log(rightitem);
-                }
-                console.log('next');
-                
-                $('.left-pos-2').removeClass('left-pos-2').addClass('back-pos');
-                $('.left-pos').removeClass('left-pos').addClass('left-pos-2');
-                  $('.main-pos').removeClass('main-pos').addClass('left-pos');
-            $('.right-pos').removeClass('right-pos').addClass('main-pos');
-                $('.right-pos-2').removeClass('right-pos-2').addClass('right-pos');
-                $('#'+rightitem).removeClass('back-pos').addClass('right-pos-2');
+        }
+        
+        //setting the left positioned item
+        if(positionvalue == 'leftposition') {
+            //left positioned image should always be one left than main positioned image.
+            position = startItem - 1;
+        
+            //reset last image in list to left position if first image is in main position
+            if(position < 1) {
+            position = itemCount;
             }
-          }
+        }
     
-        //next button click
-        $('#next').click(function() {
-            swap('clockwise');
-        });
+        return position;
+        }  
     
-        //prev button click
-        $('#prev').click(function() {
-            swap('counter-clockwise');
-        });
-      
-      $('.left-pos-2').click(function() {
-            swap('counter-clockwise');
-        });
-    
-    }(jQuery));
+    $('#'+ startItem +'').removeClass('main-pos').addClass('left-pos');
+    $('#'+ (startItem+pos()) +'').removeClass('right-pos').addClass('main-pos');
+    $('#'+ (startItem+pos()) +'').removeClass('back-pos').addClass('right-pos');
+    $('#'+ pos('leftposition') +'').removeClass('left-pos').addClass('back-pos');
+
+        startItem++;
+        position=0;
+        if(startItem > itemCount) {
+        startItem = 1;
+        }
+    }
+    }
+
+    //next button click function
+    $('#next').click(function() {
+    swap('clockwise');
+    });
+
+    //prev button click function
+    $('#prev').click(function() {
+    swap('counter-clockwise');
+    });
+
+    //if any visible items are clicked
+    $('li').click(function() {
+    if($(this).attr('class') == 'items left-pos') {
+        swap('counter-clockwise'); 
+    }
+    else {
+        swap('clockwise'); 
+    }
+    });
 });
