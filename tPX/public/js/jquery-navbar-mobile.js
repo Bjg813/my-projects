bcdf8396db27a31456e707717dcdb5b8d804b50a
Brennangrout <bjg813@gmail.com>
2018-03-08 16:40:34 -0800

    finish mobile modal

--- tPX/public/js/jquery-navbar-mobile.js
+++ tPX/public/js/jquery-navbar-mobile.js
@@ -1,12 +1,22 @@
 $(document).ready(function(){
 
-    $(".hamburger-nav").on("click", function(){
-
+    $(".hamburger-nav").on("click", function(event){
         // $(".menu").fadeToggle("slow").toggleClass("menu-hide");
+        event.stopPropagation();
         $(".menu").animate({
           height: 'toggle'
         });
+    });
+   
+});
 
+// if user clicks outside of the menu or clicks menu item it will close mobile menu
+$(document).click( function(){
+        $(".menu").animate({
+            height: 'hide'
+        });
     });
 
-});
+
+
+
