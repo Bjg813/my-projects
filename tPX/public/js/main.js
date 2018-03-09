0ba13d5d5e56fc9f5cba8cbae3c8a12c13c6f65d
Brennangrout <bjg813@gmail.com>
2018-03-09 00:02:07 -0800

    cached images with js

--- tPX/public/js/main.js
+++ tPX/public/js/main.js
@@ -1,3 +1,24 @@
 /***************************************
                 main.js
 ***************************************/
+function preloadImages(array) {
+    if (!preloadImages.list) {
+        preloadImages.list = [];
+    }
+    var list = preloadImages.list;
+    for (var i = 0; i < array.length; i++) {
+        var img = new Image();
+        img.onload = function() {
+            var index = list.indexOf(this);
+            if (index !== -1) {
+                // remove image from the array once it's loaded
+                // for memory consumption reasons
+                list.splice(index, 1);
+            }
+        }
+        list.push(img);
+        img.src = array[i];
+    }
+}
+
+preloadImages(["../images/hero-image.jpg", "../images/../images/image-carousel-1.jpg", "../images/image-carousel-2.jpg","../images/image-carousel-3.jpg", "../images/image-carousel-4.jpg", "../images/image-carousel-5.jpg", "../images/image-carousel-6.jpg"]);
