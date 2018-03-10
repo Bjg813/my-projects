95afb93df5c2d9c24751182d78e10df6b30aae8c
Brennangrout <bjg813@gmail.com>
2018-03-09 18:06:50 -0800

    made mobile nav solid color

--- tPX/public/js/modal.js
+++ tPX/public/js/modal.js
@@ -8,6 +8,8 @@ const modal = document.getElementById('simpleModal');
 const modalBtn = document.getElementById('modalBtn');
 // Get close button
 const closeBtn = document.getElementsByClassName('closeBtn')[0];
+// Get body element
+const body = document.getElementById('body');
 
 // Listen for open click
 modalBtn.addEventListener('click', openModal);
@@ -15,10 +17,13 @@ modalBtn.addEventListener('click', openModal);
 closeBtn.addEventListener('click', closeModal);
 // Listen for outside click
 window.addEventListener('click', outsideClick);
+// Stop body scroll when modal is open
+body.addEventListener("scroll", noscroll);
 
 // Function to open modal
 function openModal() {
     modal.style.display = 'block';  
+    noscroll();
 }
 // Function to close modal
 function closeModal() {
@@ -29,4 +34,8 @@ function outsideClick(e) {
     if(e.target === modal) {
     modal.style.display = 'none';
     }
+}
+
+function noscroll() {
+    body.scrollTo(0,0);
 }
